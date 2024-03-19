223d5e6b27b64c87fdc8253179b650fb465bd9ba
waqas <wmunir232@gmail.com>
2024-03-19 22:42:03 +0500

    Cleaned Up everything

--- Java Baiscs/src/Overloading.java
+++
@@ -1,5 +0,0 @@
-public class Overloading {
-    public static void main(String[] args) {
-//        Method overloading means defining the methods of same name with different number of parameters
-    }
-}
