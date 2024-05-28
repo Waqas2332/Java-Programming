4dcd10b5ef88412205b20d20ddfd514c9af40207
waqas <wmunir232@gmail.com>
2024-05-28 19:53:06 +0500

    Solved 1 Leetcode Problem

---
+++ Basic/main.py
@@ -0,0 +1,14 @@
+def subtractProductAndSum(n: int) -> int:
+    sum = 0
+    product = 1
+
+    while (n > 0):
+        sum += n % 10
+        product *= n % 10
+
+        n = n // 10
+
+    return product - sum
+
+
+print(subtractProductAndSum(234))
